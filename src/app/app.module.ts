9b727ae293b87dc8d4a90203d9bc085575cf1445
Caio Bruno <123472004+CaioBrunoE@users.noreply.github.com>
2024-06-11 10:33:45 -0300

    Interceptor para requisições Http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
+import { AuthInterceptorProvinder } from './interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -77,7 +78,7 @@ import { ToastrModule } from 'ngx-toastr';
       progressBar:true
     })
   ],
-  providers: [],
+  providers: [AuthInterceptorProvinder],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
