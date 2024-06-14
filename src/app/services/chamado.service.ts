52dd2df870fa58b71670c57eb7936b7d1221fce3
Caio Bruno <123472004+CaioBrunoE@users.noreply.github.com>
2024-06-14 16:16:29 -0300

    Componete CHAMADO-UPDATE concluido

--- src/app/services/chamado.service.ts
+++ src/app/services/chamado.service.ts
@@ -8,8 +8,12 @@ import { API_CONFIG } from '../config/api.config';
   providedIn: 'root'
 })
 export class ChamadoService {
+  
 
   constructor(private http : HttpClient) { }
+  findById(id: any): Observable<Chamado> {
+    return this.http.get<Chamado>(`${API_CONFIG.baseUrl}/chamados/${id}`);
+  }
 
   findAll(): Observable<Chamado[]>{
    return this.http.get<Chamado[]>(`${API_CONFIG.baseUrl}/chamados`)
@@ -18,4 +22,9 @@ export class ChamadoService {
   create(chamado: Chamado): Observable<Chamado>{
   return this.http.post<Chamado>(`${API_CONFIG.baseUrl}/chamados`, chamado)
   }
+
+  update(chamado: Chamado):Observable<Chamado> {
+    return this.http.put<Chamado>(`${API_CONFIG.baseUrl}/chamados/${chamado.id}`, chamado)
+  }
+  
 }
